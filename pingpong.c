929bbc35f15c8502bc97c7d8c0c0669c88e03e4c
vincnx <vincent.celcio@gmail.com>
2022-11-29 16:32:17 +0700

    ganti file yang benar

--- pingpong.c
+++ pingpong.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
+#include <time.h>
 
 typedef struct Base{
     int x;
@@ -11,24 +12,42 @@ typedef struct Base{
     int point;
     char k_up;
     char k_dwn;
-} Base;
+}Base;
+
 typedef struct Ball{
     int x;
     int y;
     int x_mov;
     int y_mov;
-} Ball;
+}Ball;
 
-int setup();
+void _2p();
+void _1p();
+
 void draw_map();
 void draw_base();
+Base move_base();
 void draw_ball();
-void move_base();
-void move_ball();
+Ball move_ball();
+Ball hit_map();
+Ball hit_base();
+Base not_hit();
+void draw_point();
+
+void delay();
 void gotoxy();
 
+int play = 0;
+int ball_p = 0;
+int hit_x = 0;
+
 int main(){
-    int cond;
+    _1p();
+}
+
+void _1p(){
+    srand(time(NULL));
+    int i, j, key, rand_num;
     Base player[2];
     Ball ball;
     player[0].x = 6;
@@ -49,119 +68,278 @@ int main(){
     player[1].point = 0;
     ball.x = 7;
     ball.y = 13;
-    ball.x_mov = 0;
-    ball.y_mov = 0;
-    cond = setup(25, 75, 2, 0, &player, &ball);
     
-}
-
-int setup(int map_h, int map_w, int total_player, int play_ball, Base *players, Ball *ball){
-    int i, key;
-    int game_over = 0; int start = 0;
-    while (!game_over){
+    while(player[0].point + player[1].point != 5){
+        a: 
+        rand_num = rand() % 10 + 1;
         system("cls");
-        draw_map(map_h, map_w);
+        draw_map(25, 75);
         draw_ball(ball);
         if (kbhit()){
             key = getch();
         }
-        if (key == 32){
-            start = 1;
-        }
-        for (i = 0; i < total_player; i++){
-            draw_base(&players[i]);
-            move_base(&players[i], key);
-            // ball hit base
-            if (abs(ball->x - players[i].x) == 1){
-                if (ball->y == players[i].y + players[i].len / 2){
-                    ball->x_mov *= -1;
-                    ball->y_mov = 0;
+        for (i = 0; i < 2; i++){
+            draw_base(player[i]);
+            draw_point(player[i]);
+            player[i] = not_hit(player[i], ball);
+            if (hit_x){
+                key = 'z';
+                ball.x_mov = 0;
+                ball.y_mov = 0;
+                if (ball.x == 2){
+                    ball.x = 7;
+                    ball.y = 13;
+                    hit_x = 0;
                 }
-                else if (ball->y >= players[i].y && ball->y < players[i].y + players[i].len /2){
-                    ball->x_mov *= -1;
-                    ball->y_mov = -1;
+                else{
+                    ball.x = 69;
+                    ball.y = 13;
+                    hit_x = 0;
                 }
-                else if (ball->y < players[i].y + players[i].len){
-                    ball->x_mov *= -1;
-                    ball->y_mov = 1;
+                for (j = 0; j < 2; j++){
+                    player[j].y = 11;
+                    player[j].y_mov = 0;
                 }
+                goto a;
             }
-            // end of ball hit base
+            ball = hit_base(ball, player[i], play);
         }
-        if (ball->x == 2){
-            Sleep(1500);
-            return 0;
+        player[0] = move_base(player[0], key, 0);
+        player[1] = move_base(player[1], key, 1);
+        ball = hit_map(ball, play);
+        ball = move_ball(ball, player[ball_p], key, play);
+        if (key == 32){
+            play = 1;
         }
-        else if (ball->x == 74){
-            Sleep(1500);
-            return 1;
-        }
-
-        // ball mov
-        if (ball->y == 2 || ball->y == 24){
-            ball->y_mov *= -1;
-        }
-        else if (start && ball->x_mov == 0){
-            ball->x_mov = 1 + 2 * play_ball;
-            ball->y_mov = players[play_ball].y_mov;
-        }
-        else if (!start){
-            ball->y_mov = players[play_ball].y_mov;
-        }
-        ball->x += ball->x_mov;
-        ball->y += ball->y_mov;
-        // end of ball mov
-
-        gotoxy(80, 12);
-        printf("%i %i", ball->x, players[1].x);
-
+        delay();
     }
 }
 
-void move_base(Base *players, int key){
-    if (players->y == 2 || players->y == 20)
-    {
-        players->y_mov = 0;
+
+
+
+
+
+
+
+void _2p(){
+    int i, j, key;
+    Base player[2];
+    Ball ball;
+    player[0].x = 6;
+    player[0].y = 11;
+    player[0].len = 5;
+    player[0].y_mov = 0;
+    player[0].num = 0;
+    player[0].k_up = 'w';
+    player[0].k_dwn = 's';
+    player[0].point = 0;
+    player[1].x = 70;
+    player[1].y = 11;
+    player[1].len = 5;
+    player[1].y_mov = 0;
+    player[1].num = 1;
+    player[1].k_up = 'i';
+    player[1].k_dwn = 'k';
+    player[1].point = 0;
+    ball.x = 7;
+    ball.y = 13;
+    
+    while(player[0].point + player[1].point != 5){
+        a: 
+        system("cls");
+        draw_map(25, 75);
+        draw_ball(ball);
+        if (kbhit()){
+            key = getch();
+        }
+        for (i = 0; i < 2; i++){
+            draw_base(player[i]);
+            draw_point(player[i]);
+            player[i] = not_hit(player[i], ball);
+            if (hit_x){
+                key = 'z';
+                ball.x_mov = 0;
+                ball.y_mov = 0;
+                if (ball.x == 2){
+                    ball.x = 7;
+                    ball.y = 13;
+                    hit_x = 0;
+                }
+                else{
+                    ball.x = 69;
+                    ball.y = 13;
+                    hit_x = 0;
+                }
+                for (j = 0; j < 2; j++){
+                    player[j].y = 11;
+                    player[j].y_mov = 0;
+                }
+                goto a;
+            }
+            ball = hit_base(ball, player[i], play);
+            player[i] = move_base(player[i], key, 0);
+        }
+        ball = hit_map(ball, play);
+        ball = move_ball(ball, player[ball_p], key, play);
+        if (key == 32){
+            play = 1;
+        }
+        delay();
     }
-    if (key == players->k_up && players->y != 2)
-    {
-        players->y_mov = -1;
-    }
-    else if (key == players->k_dwn && players->y != 20)
-    {
-        players->y_mov = 1;
-    }
-    players->y += players->y_mov;
 }
 
-void draw_ball(Ball *ball){
-    gotoxy(ball->x, ball->y);
+void draw_point(Base player){
+    gotoxy(80, 12);
+    printf("score :");
+    gotoxy(80, 13 + player.num);
+    printf("player %i : %i  %i", player.num, player.point, play);
+}
+
+
+Base not_hit(Base player, Ball ball){
+    Base temp;
+    temp = player;
+    if (ball.x == 2 && temp.num == 1){
+        temp.point ++;
+        play = 0;
+        ball_p = 0;
+        hit_x = 1;
+    }
+    else if (ball.x == 74 && temp.num == 0){
+        temp.point ++;
+        play = 0;
+        ball_p = 1;
+        hit_x = 1;
+    }
+    return temp;
+}
+
+
+Ball hit_base(Ball ball, Base player, int play){
+    Ball temp;
+    temp = ball;
+    if (play){
+        if (abs(player.x - ball.x) == 1 && player.y <= ball.y && ball.y  < player.y + player.len){
+            if (ball.y < player.y + player.len / 2){
+                temp.y_mov = -1;
+                temp.x_mov *= -1;
+            }
+            else if (ball.y == player.y + player.len / 2){
+                temp.y_mov = 0;
+                temp.x_mov *= -1;
+            }
+            else {
+                temp.y_mov = 1;
+                temp.x_mov *= -1;
+            }
+            return temp;
+        }
+    }
+}
+
+
+Ball hit_map(Ball ball, int play){
+    Ball temp;
+    int i;
+    temp = ball;
+    if (play){
+        if (temp.y == 2 || temp.y == 24){
+            temp.y_mov *= -1;
+            temp.y = ball.y + temp.y_mov;
+        }
+    }
+    return temp;
+}
+
+
+Ball move_ball(Ball ball, Base player, int key, int play){
+    Ball temp;
+    temp = ball;
+    if (!play){
+        if (abs(player.x - temp.x) == 1){
+            if (key == 32){
+                temp.y_mov = player.y_mov;
+                temp.x_mov = 1 - (2 * player.num);
+            }
+            else{
+                temp.x_mov = 0;
+                temp.y_mov = player.y_mov;
+            }
+        }
+    }
+    temp.x += temp.x_mov;
+    temp.y += temp.y_mov;
+    return temp;
+}
+
+
+void draw_ball(Ball ball){
+    gotoxy(ball.x, ball.y);
     printf("*");
 }
 
-void draw_base(Base *player){
+
+Base move_base(Base player, int key, int num_player){
+    Base tmp;
+    tmp = player;
+    if (num_player == 1 && tmp.y_mov == 0){
+        tmp.y_mov = -1;
+    }
+    else if (num_player == 0){
+        if (key == tmp.k_up){    
+            tmp.y_mov = -1;
+        }
+        else if (key == tmp.k_dwn){
+            tmp.y_mov = 1;
+        }
+    }
+    if (tmp.y == 2 && tmp.y_mov == -1){
+        tmp.y_mov = 0;
+        if (num_player == 1){
+            tmp.y_mov = 1;
+        }
+    }
+    else if (tmp.y == 25 - tmp.len && tmp.y_mov == 1){
+        tmp.y_mov = 0;
+        if (num_player == 1){
+            tmp.y_mov = -1;
+        }
+    }
+    tmp.y += tmp.y_mov;
+    return tmp;
+}
+
+
+void draw_base(Base player){
     int i;
-    for (i = 0; i < player->len; i++)
-    {
-        gotoxy(player->x, player->y + i);
+    for (i = 0; i < player.len; i++){
+        gotoxy(player.x, player.y + i);
         printf("|");
     }
 }
 
 void draw_map(int map_h, int map_w){
     int i, j;
-    for (i = 1; i <= map_h; i++)
-    {
-        for (j = 1; j <= map_w; j++)
-        {
-            if (i == 1 || i == map_h || j == 1 || j == map_w / 2 + 1 || j == map_w)
-            {
+    for (i = 1; i <= map_h; i++){
+        for (j = 1; j <= map_w; j++){
+            if (i == 1 || i == map_h || j == 1 || j == map_w / 2 + 1 || j == map_w){
                 gotoxy(j, i);
                 printf("#");
             }
         }
     }
 }
+
+
+
+void delay(){
+    int i, j;
+    for (i = 0; i < 1000; i++){
+        for (j = 0; j < 7000; j++);
+    }
+}
+
 
 void gotoxy(int x, int y){
     COORD coor;
